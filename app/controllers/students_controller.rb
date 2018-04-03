2b76b2043f41adaee999a14fe4243d563bb2541e
Alexander Spanos <alexisspa9@gmail.com>
2018-04-03 17:34:34 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,8 +9,8 @@ class StudentsController < ApplicationController
     set_student
   end
   def activate
-  set_student
-    @student.active = !@student.active 
+    set_student
+    @student.active = !@student.active
     @student.save
     redirect_to student_path(@student)
   end
